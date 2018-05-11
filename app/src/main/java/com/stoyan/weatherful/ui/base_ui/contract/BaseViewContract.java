9505f7964b0f30e128a0d45be0cf576a698010d8
Stoyan-Ivanov <stoyaniviv@gmail.com>
2018-05-11 18:07:25 +0300

    Deleting useless mvp presenters and interfaces

--- app/src/main/java/com/stoyan/weatherful/ui/base_ui/contract/BaseViewContract.java
+++
@@ -1,8 +0,0 @@
-package com.stoyan.weatherful.ui.base_ui.contract;
-
-/**
- * Created by stoyan.ivanov2 on 3/27/2018.
- */
-
-public interface BaseViewContract {
-}
--- app/src/main/java/com/stoyan/weatherful/ui/base_ui/presenter/BasePresenterContract.java
+++
@@ -1,9 +0,0 @@
-package com.stoyan.weatherful.ui.base_ui.presenter;
-
-/**
- * Created by stoyan.ivanov on 3/19/2018.
- */
-
-public interface BasePresenterContract {
-    void onViewDestroy();
-}
